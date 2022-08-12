c4352527115b1762a06591ea02083f469fc15a9f
Alexander Blaschuk <blaschuk95@gmail.com>
2022-08-12 13:18:22 +0300

    update

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -1,15 +1,6 @@
-requirejs(['ext_editor_io', 'jquery_190'],
+requirejs(['ext_editor_io2', 'jquery_190'],
     function (extIO, $) {
-        
-        var $tryit;
-
-        var io = new extIO({
-            multipleArguments: true,
-            functions: {
-                python: 'second_index',
-                js: 'secondIndex'
-            }
-        });
+        var io = new extIO({});
         io.start();
     }
 );
